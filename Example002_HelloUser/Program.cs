468445c7a87455bd186a15442a9b909541c3db8f
Olga Vashchenkova <o.vashchenkova@gmail.com>
2022-08-05 01:02:30 +0300

    new

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter name:");
+string username = Console.ReadLine(); 
+Console.WriteLine("Hello,");
+Console.WriteLine(username);
---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,4 @@
+﻿int numberA = 5;
+int numberB = 6;
+Console.WriteLine(numberA + numberB);
+
